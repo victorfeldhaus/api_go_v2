287bf79c976d6a15e64df01a7905e18715d83c13
Victor Feldhaus <victorfeldhaus@Victors-MacBook-Air.local>
2024-12-03 23:57:01 -0300

    create structure of project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/victorfeldhaus/api_go_v2
+
+go 1.23.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+
+	fmt.Println(os.Getenv("TEST"))
+
+}
